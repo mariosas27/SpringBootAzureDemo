46bb1aee584e5e536ec38c863c9bb1ef1e94ede6
Juan Mario Sastre Cuahutle <mariosas27@gmail.com>
2023-05-29 23:45:01 -0500

    first commit

---
+++ src/main/java/com/example/SpringBootAzureDemo/SpringBootAzureDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootAzureDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAzureDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAzureDemoApplication.class, args);
+	}
+
+}
